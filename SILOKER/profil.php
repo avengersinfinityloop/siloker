3ed007b442576fd89cb0debe749ec7b999e50499
Sidik <sidiknurizki@gmail.com>
2022-07-20 22:48:40 +0700

    test

--- SILOKER/profil.php
+++ SILOKER/profil.php
@@ -25,6 +25,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <!-- Dropdown -->
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
@@ -60,15 +61,6 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">LOGIN</a>
-                </li>
-                <!-- Dropdown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="NavbarDrop" role="button" data-bs-toggle="dropdown" aria-expanded="false">DropDown</a>
-                    <div class="dropdown-menu" aria-labelledby="NavbarDrop">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
                 </li>
             </ul>
         </div>
